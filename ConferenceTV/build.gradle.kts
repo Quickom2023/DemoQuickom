ee93e3cd6bfa16b132f828be16d5f8397cd7d4c0
Kinh Tran Vinh <tranvinhkinh1@gmail.com>
2026-04-29 17:33:29 +0700

    update project settings

--- ConferenceTV/build.gradle.kts
+++ ConferenceTV/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     // THÊM DÒNG NÀY cho Compose Compiler
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library) apply false
 }
--- ConferenceTV/settings.gradle.kts
+++ ConferenceTV/settings.gradle.kts
@@ -24,4 +24,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "DemoApp"
 include(":app")
- 
+include(":quickom-sdk")
